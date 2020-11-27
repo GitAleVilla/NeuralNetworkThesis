9235de8269888ccfdb7e2282795855027ef7daaa
Alessandro Villa <villaalessandro16@gmail.com>
2020-11-27 14:04:04 +0100

    Dummy test

--- HTReader.py
+++ HTReader.py
@@ -12,7 +12,8 @@ from kerastuner.tuners import RandomSearch
 from kerastuner.engine.hyperparameters import HyperParameters
 
 import matplotlib.pyplot as plt
-
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 import pickle
 
 
@@ -63,6 +64,7 @@ def model_builder(hp):
 	return model
 #################################
 
+############################## Best model and train
 with open('HTHist_ch1000.pkl','rb') as f:
 	tuned = pickle.load(f)
 
@@ -93,4 +95,52 @@ plt.show()
 plt.plot(epoch, acc, label='accuracy')
 plt.plot(epoch, val_acc, label='val_accuracy')
 plt.legend()
+plt.show()
+
+############################## Prediction
+prediction = best_model.predict(val_wf)
+pred_label = np.argmax(prediction, axis=1)
+minx = np.min(val_label) - 0.5
+maxx = np.max(val_label) + 0.5
+miny = np.min(pred_label) - 0.5
+maxy = np.max(pred_label) + 0.5
+#print(prediction[0])
+#print(val_label[0])
+#print(len(np.argmax(prediction, axis=1)))
+#print(len(val_label))
+
+############################## Plot
+fig, ax = plt.subplots(figsize =(8, 6))
+
+# create heatmap
+conf_matrix = confusion_matrix(val_label, pred_label)
+print(conf_matrix)
+normalizer = conf_matrix.sum(axis=0)
+print(normalizer)
+conf_matrix_norm = conf_matrix / normalizer
+print(conf_matrix_norm)
+hmap = sns.heatmap(conf_matrix_norm, 
+				   annot = True,
+				   cmap = plt.cm.Blues,
+				   fmt='.1%',
+				   ax = ax)
+
+# add labels
+ax.set_xlabel('True label')
+ax.set_ylabel('Predicted label')
+ax.set_xticks([0.5,1.5,2.5])
+ax.set_xticklabels(['$e^-$','$\pi^-$','$e^- + \pi^-$'])
+plt.yticks([0.5,1.5,2.5], rotation=0, va='center')
+ax.set_yticklabels(['$e^-$','$\pi^-$','$e^- + \pi^-$'])
+
+# lavel colorbar
+cbar = hmap.collections[0].colorbar
+cbar.set_ticks([0.,0.25,0.5,0.75,1.])
+cbar.set_ticklabels(['0.0%','25.0%','50.0%','75.0%','100.0%'])
+
+# add border
+for _, spine in hmap.spines.items():
+    spine.set_visible(True)
+
+plt.tight_layout()
 plt.show()
