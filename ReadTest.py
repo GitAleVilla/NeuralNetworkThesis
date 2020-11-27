213649d674bec1f06feb8da403a8c43cffe9ceec
Alessandro Villa <villaalessandro16@gmail.com>
2020-11-27 11:26:55 +0100

    Cherenkov nice

--- ReadTest.py
+++ ReadTest.py
@@ -2,6 +2,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 ############################## Import model
 model = keras.models.load_model('../../SavedModel/model_cher1000')
@@ -46,20 +48,40 @@ print(y_bins)
 
 fig, ax = plt.subplots(figsize =(8, 6))
 
-plt.hist2d(x = val_label, y = pred_label,
+# create heatmap
+conf_matrix = confusion_matrix(val_label, pred_label)
+print(conf_matrix)
+normalizer = conf_matrix.sum(axis=0)
+print(normalizer)
+conf_matrix_norm = conf_matrix / normalizer
+print(conf_matrix_norm)
+hmap = sns.heatmap(conf_matrix_norm, 
+				   annot = True,
+				   cmap = plt.cm.Blues,
+				   fmt='.1%',
+				   ax = ax)
+
+'''plt.hist2d(x = val_label, y = pred_label,
 		   bins = [x_bins, y_bins],
-		   cmap = plt.cm.Blues)
+		   cmap = plt.cm.Blues)'''
 
-#print(xbins)
-
+# add labels
 ax.set_xlabel('True label')
 ax.set_ylabel('Predicted label')
-ax.set_xticks([0,1,2])
+ax.set_xticks([0.5,1.5,2.5])
 ax.set_xticklabels(['$e^-$','$\pi^-$','$e^- + \pi^-$'])
-ax.set_yticks([0,1,2])
+plt.yticks([0.5,1.5,2.5], rotation=0, va='center')
 ax.set_yticklabels(['$e^-$','$\pi^-$','$e^- + \pi^-$'])
 
-plt.colorbar()
+cbar = hmap.collections[0].colorbar
+cbar.set_ticks([0.,0.25,0.5,0.75,1.])
+cbar.set_ticklabels(['0.0%','25.0%','50.0%','75.0%','100.0%'])
+
+# add border
+for _, spine in hmap.spines.items():
+    spine.set_visible(True)
+
+#plt.colorbar()
 plt.tight_layout()
 plt.show()
 
